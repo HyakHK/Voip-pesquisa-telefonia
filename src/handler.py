8dc9f73c2bf359fb40260a9c4fe2baf6da9e0ac1
victorm304 <143431343+victorm304@users.noreply.github.com>
2025-12-03 14:55:08 -0300

    Update handler.py

--- src/handler.py
+++ src/handler.py
@@ -2,18 +2,30 @@
 
 from asterisk.agi import *
 from suap_client import *
+from agi_handler import AgiHandler
 
-# ** Código incompleto ** 
+# Incompleto
 
 def handler():
     agi = AGI()
+    agi_handler = AgiHandler(agi)
+    
     agi.verbose("Inicio.")
     agi.answer()
     
-    res = agi.get_data("beep", timeout=20000, max_digits=100) # captura os digitos do usuário
-    digits = res
+    agi.verbose("A obter matrícula e código de acesso...")
+    try:
+        enrolment, access_code = agi_handler.ask_enrolment_and_access_code()
+        agi.verbose(f"Matrícula: {enrolment}, Código de acesso: {access_code}")
 
-    suap = SuapClient(enrolment="", responsible_code="") # sua matrícula e código de acesso aí
+    except Exception as e:
+        agi.verbose(f"Erro: {e}")
+
+    suap = SuapClient(
+        enrolment= enrolment, 
+        responsible_code = access_code
+    ) 
+    
     boletim = suap.get_boletim()
 
     agi.hangup()
