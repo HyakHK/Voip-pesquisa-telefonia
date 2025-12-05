c55b53ca2021cc039add2a7cb44102fed48966c6
victorm304 <143431343+victorm304@users.noreply.github.com>
2025-12-04 22:14:09 -0300

    ...

--- src/main.py
+++ src/main.py
@@ -2,34 +2,45 @@
 
 from asterisk.agi import *
 from suap_client import *
+from utils import *
 from agi_handler import AgiHandler
+from text_to_speech import text_to_speech
 
 # Incompleto
 
 def handler():
-    agi = AGI()
-    agi_handler = AgiHandler(agi)
-    
-    agi.verbose("Inicio.")
-    agi.answer()
-    
-    agi.verbose("A obter matrícula e código de acesso...")
-    try:
-        enrolment, access_code = agi_handler.ask_enrolment_and_access_code()
-        agi.verbose(f"Matrícula: {enrolment}, Código de acesso: {access_code}")
+   try:
+        agi = AGI()
+        agi_handler = AgiHandler(agi)
+        
+        agi.verbose("Inicio.")
+        agi.answer()
+        
+        agi.verbose("A obter matrícula e código de acesso...")
+        try:
+            enrolment, access_code = agi_handler.ask_enrolment_and_access_code()
+            agi.verbose(f"Matrícula: {enrolment}, Código de acesso: {access_code}")
 
-    except Exception as e:
+        except Exception as e:
+            agi.verbose(f"Erro: {e}")
+
+        suap = SuapClient(
+            enrolment= enrolment, 
+            responsible_code = access_code
+        ) 
+        
+        boletim = suap.get_boletim()
+        text_boletim = format_boletim(boletim)
+        audio_wav = text_to_speech(text_boletim)
+        audio_gsm = wav_to_gsm(audio_wav)
+
+        agi.stream_file(audio_gsm.split(".gsm")[0])
+
+        
+        agi.hangup()
+    
+   except Exception as e:
         agi.verbose(f"Erro: {e}")
-
-    suap = SuapClient(
-        enrolment= enrolment, 
-        responsible_code = access_code
-    ) 
-    
-    boletim = suap.get_boletim()
-
-    agi.hangup()
-
 
 if __name__ == "__main__":
     handler()
